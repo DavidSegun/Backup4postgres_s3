d3e86750d5a807708a5f9fda7dff8b755927ce6d
David Segun <segundavidk@gmail.com>
2023-07-30 05:33:29 +0100

    Add files via upload

---
+++ postgres-s3-backups-main/src/env.ts
@@ -0,0 +1,21 @@
+import { envsafe, str } from "envsafe";
+
+export const env = envsafe({
+  // AWS_ACCESS_KEY_ID: str(),
+  // AWS_SECRET_ACCESS_KEY: str(),
+  // AWS_S3_BUCKET: str(),
+  // AWS_S3_REGION: str(),
+  DATABASE_URL: str({
+    desc: "The connection string of the database to backup.",
+  }),
+  BACKUP_CRON_SCHEDULE: str({
+    desc: "The cron schedule to run the backup on.",
+    default: "*/10 * * * *",
+    allowEmpty: true,
+  }),
+  AWS_S3_ENDPOINT: str({
+    desc: "The S3 custom endpoint you want to use.",
+    default: "",
+    allowEmpty: true,
+  }),
+});
